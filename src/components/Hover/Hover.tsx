2b2750db1824b4ba36861f77694575ffeabed4dc
Vikash  Gupta <vikashgupta@ip-192-168-43-119.ap-southeast-1.compute.internal>
2021-11-09 15:45:59 +0530

    initial commit

---
+++ src/components/Hover/Hover.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Hover.module.css';
+import cx from 'classnames';
+
+const Hover = ({ children, disabled = false, size }: any) => {
+  const sizeStyle: any = {
+    small: styles.small,
+  };
+  return (
+    <div className={cx(styles.wrapper, disabled ? styles.disableHoverState : '', sizeStyle[size])}>
+      {children}
+    </div>
+  )
+}
+
+export default Hover;
