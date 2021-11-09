d70c37129343e40a6fb3f11781218db6cfa95a0d
Vikash  Gupta <vikashgupta@ip-192-168-43-119.ap-southeast-1.compute.internal>
2021-11-10 01:15:02 +0530

    wip: temp commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import ReactGA from 'react-ga'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ReactGA.set({ page: url })
+      ReactGA.pageview(url)
+    }
+    ReactGA.initialize('G-9T470HFNJE', { debug: process.env.NODE_ENV === 'development' });
+    ReactGA.set({ page: router.pathname })
+    ReactGA.pageview(router.pathname)
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
 
